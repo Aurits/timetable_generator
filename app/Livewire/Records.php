b32d3e84edcc1a38091efa499505494bcaaab7b2
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-02 13:42:35 +0000

    Jan 2, 2024, 4:42 PM

--- app/Livewire/Records.php
+++ app/Livewire/Records.php
@@ -8,15 +8,4 @@ use App\Models\TimetableEntry;
 class Records extends Component
 {
     public $entries;
-
-    public function mount()
-    {
-        // Fetch records from the database and store them in $this->entries
-        $this->entries = TimetableEntry::paginate(10); // Change 10 to the number of records per page you desire
-    }
-
-    public function render()
-    {
-        return view('livewire.records');
-    }
 }
