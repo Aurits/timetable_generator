64950b80470e131c3d54b59343fdb778448e317a
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-02 13:24:57 +0000

    Jan 2, 2024, 4:24 PM

--- resources/views/livewire/timetable-record.blade.php
+++ resources/views/livewire/timetable-record.blade.php
@@ -1,3 +1,15 @@
+<!-- resources/views/livewire/timetable-record.blade.php -->
+
 <div>
-    {{-- Be like water. --}}
+    @if ($isEditing)
+    <form wire:submit.prevent="update">
+        <!-- Edit form fields go here -->
+        <button type="submit" class="btn btn-primary">Save Changes</button>
+    </form>
+    @else
+    <!-- Display record details -->
+
+    <button wire:click="edit" class="btn btn-primary">Edit</button>
+    <button wire:click="delete" class="btn btn-danger">Delete</button>
+    @endif
 </div>
