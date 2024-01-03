b7797f48eb7c5517f589bf9d392ae8907f835187
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-03 09:35:27 +0000

    Jan 3, 2024, 12:35 PM

--- resources/views/livewire/archive.blade.php
+++ resources/views/livewire/archive.blade.php
@@ -37,33 +37,10 @@
                             @endforeach
                         </tr>
                     </thead>
-                    <tbody>
-                        @foreach(['7:00 AM - 8:00 AM', '8:00 AM - 9:00 AM', '9:00 AM - 10:00 AM',
-                        '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM',
-                        '1:00 PM - 2:00 PM', '2:00 PM - 3:00 PM', '3:00 PM - 4:00 PM', '4:00 PM - 5:00 PM'] as
-                        $timeSlot)
-                        <tr>
-                            <th class="text-center">{{ $timeSlot }}</th>
-                            @foreach(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] as $day)
-                            <?php
-                            $entry = $timetableEntries->first(function ($entry) use ($day, $timeSlot) {
-                                return $entry->day == $day && $entry->time_slot == $timeSlot;
-                            });
-                            ?>
-                            <td>
-                                @if ($entry)
-                                {{ $entry->subject->name }}<br>
-                                {{ $entry->teacher->name }}
-                                @endif
-                            </td>
-                            @endforeach
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @else
-                <p class="text-center">No timetable entries available for {{ $class->name }}.</p>
-                @endif
+
+                    @else
+                    <p class="text-center">No timetable entries available for {{ $class->name }}.</p>
+                    @endif
             </div>
         </div>
     </div>
