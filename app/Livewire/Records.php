e67786b5434cc0842414a0140f1504a54f16f75e
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-03 07:43:23 +0000

    Jan 3, 2024, 10:43 AM

--- app/Livewire/Records.php
+++ app/Livewire/Records.php
@@ -37,9 +37,4 @@ class Records extends Component
             $this->deleteSuccessMessage = 'Record deleted successfully.';
         }
     }
-
-    public function cancelDelete()
-    {
-        $this->reset(['entryToDelete', 'deleteSuccessMessage']);
-    }
 }
