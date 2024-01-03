72a01c091ec23cc7e8a588b83b2d92245894f689
Ambrose Alanda <alandaambrose@gmail.com>
2024-01-03 09:32:46 +0000

    Jan 3, 2024, 12:32 PM

--- resources/views/livewire/archive.blade.php
+++ resources/views/livewire/archive.blade.php
@@ -38,7 +38,10 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($timeSlots as $timeSlot)
+                        @foreach(['7:00 AM - 8:00 AM', '8:00 AM - 9:00 AM', '9:00 AM - 10:00 AM',
+                        '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM',
+                        '1:00 PM - 2:00 PM', '2:00 PM - 3:00 PM', '3:00 PM - 4:00 PM', '4:00 PM - 5:00 PM'] as
+                        $timeSlot)
                         <tr>
                             <th class="text-center">{{ $timeSlot }}</th>
                             @foreach(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] as $day)
